dd4d55e718174feb46cfe8f5335654b2a2fb3eed
flpalma <felipe.lp27@gmail.com>
2020-01-18 11:08:45 -0300

    primeiro commit

---
+++ src/main/java/br/com/secia/apisecia/ApiSeciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.secia.apisecia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSeciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSeciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/secia/apisecia/ApiSeciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.secia.apisecia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSeciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
